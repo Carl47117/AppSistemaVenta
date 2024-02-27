42e80df9fec568934ac571c415e4881fa7aad313
Carlos <car.elt94@gmail.com>
2024-02-26 23:01:21 -0600

    Nuevos cambios ventas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AppSistemaVenta'`, () => {
+  it(`should have as title 'App Sistema Administracion'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AppSistemaVenta');
+    expect(app.title).toEqual('App Sistema Administracion');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AppSistemaVenta');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, App Sistema Administracion'
+    );
   });
 });
